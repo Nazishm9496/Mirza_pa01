a2f0aa2e1d219dd1362901a0beade7be7a43c72b
Nazishm9496 <60050056+Nazishm9496@users.noreply.github.com>
2020-01-18 22:20:32 -0500

    First

---
+++ Mirza_pa01/src/mirza_pa01/GoodbyeWorld.java
@@ -0,0 +1,9 @@
+package mirza_pa01;
+
+public class GoodbyeWorld 
+{
+	public static void main(String[] args) 
+	{
+		System.out.printf("%s", "Goodbye, World!");
+	}
+}
---
+++ Mirza_pa01/src/mirza_pa01/HelloWorld.java
@@ -0,0 +1,9 @@
+package mirza_pa01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, World!");
+	}
+}
